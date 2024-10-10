7ac4e452998ff28e0e77a077aebb10f2d858e2a3
pongsatornp11 <pongsatorn.p11@gmail.com>
2024-10-10 08:12:11 +0700

    add login

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Hello world")
+
+print('Please Loing:')
+username = input('Username:')
+password = input('Password:')
+
+if username =='admin' and password=='1234':
+    print('Welcome')
+else:
+    print('Exit...')
